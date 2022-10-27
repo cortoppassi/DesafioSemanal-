896b5ae78add62b7ebe829cb0f001bf519a5728e
Jonathan Cortoppassi <106403752+cortoppassi@users.noreply.github.com>
2022-10-26 21:08:19 -0300

    Atualização Projeto da semana

--- js/script.js
+++ js/script.js
@@ -11,32 +11,53 @@ function adicionarRegistro() {
     };
     listadeRegistro.push(registro)
     listar();
+
+    
 };
 function listar() {
-    
     var li = document.createElement('li');
     var listaHTML = document.getElementById('listaHTML')
-    listaHTML.append(li);
+    listaHTML.appendChild(li);
 
     var btnExcluir = document.createElement("button")
     listaHTML.appendChild(btnExcluir)
     btnExcluir.innerHTML = "Exluir"
-    btnExcluir.onclick = () => { excluir()};
+    btnExcluir.setAttribute("id", 'excluir')
+    document.getElementById('excluir').addEventListener("click", excluir)
 
-    const metodoMap = listadeRegistro.map((registro) => {
-        li.append(registro.produto)
-        if(registro.produto == registro.produto){
-            li.append(registro.preco)
-        }
-        if(registro.preco == registro.preco){
-            li.append(registro.quantidade)
-        }
-    })
+    const produto = listadeRegistro.map((item) => {
+       return (item.produto) + (item.preco) + (item.quantidade)
+    });
+    li.innerHTML = (produto)
+    console.log(produto)
+};
+ const excluir = () => {
+        alert('ok')
+    };
+
+// function listar() {
     
-    console.log(metodoMap)
-};
+//     var li = document.createElement('li');
+//     var listaHTML = document.getElementById('listaHTML')
+//     listaHTML.append(li);
 
-function excluir() {
-    listadeRegistro.pop()
+//     var btnExcluir = document.createElement("button")
+//     listaHTML.appendChild(btnExcluir)
+//     btnExcluir.innerHTML = "Exluir"
+//     btnExcluir.onclick = () => { excluir()};
 
-};
+//     const metodoMap = listadeRegistro.map((registro) => {
+//         li.append(registro.produto)
+//         if(registro.produto == registro.produto && registro.preco == registro.preco){
+//             li.append(registro.preco)
+//             li.append(registro.quantidade)
+//         }
+//     })
+    
+//     console.log(metodoMap)
+// };
+
+// function excluir() {
+//     listadeRegistro.pop()
+
+// };
